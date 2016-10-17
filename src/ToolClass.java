4ed1c9e9430b25f58d66ccb3ee893ca925f07f01
jamesthall90 <jamesthall90@gmail.com>
2016-10-17 17:44:13 -0400

    - Created a ToolClass to house createImageIcon an any other utility methods that multiple classes may need to use - so as to prevent duplicating any code throughout the classes

---
+++ src/ToolClass.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+/**
+ * Created by TylerHall on 10/17/16.
+ */
+public class ToolClass {
+
+
+    protected ImageIcon createImageIcon(String path, String description) {
+//        java.net.URL imgURL = getClass().getResource(path);
+        URL imgURL = null;
+
+        try {
+            imgURL = new URL(path);
+
+        } catch (MalformedURLException e) { // catches an invalid url exception
+            e.printStackTrace();
+        }
+
+        if (imgURL != null) {
+            return new ImageIcon(imgURL, description);
+        } else {
+            System.err.println("Couldn't find file: " + path);
+            return null;
+        }
+
+    }
+}
