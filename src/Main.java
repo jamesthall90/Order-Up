9516e8a1f138995e024f664ca9acee809662047e
Yamnel <Yamnel@FapTop.local>
2016-11-27 11:23:05 -0500

    files

--- src/Main.java
+++ src/Main.java
@@ -6,12 +6,12 @@ public class Main {
 
     public static void main(String[] args) {
 
-        LogInScreen login = new LogInScreen();
+//        LogInScreen login = new LogInScreen();
 
         /* FOR TESTING */
 
 //        MainMenu menu = new MainMenu();
-//        DayPlanner day = new DayPlanner("monday");
+        DayPlanner day = new DayPlanner("monday");
       
 //          Meals test = new Meals("taco bell", "Burrito", 10, "e");
 //          System.out.println(test.toString());
